2f3ef50de177b25e8ca554dda141853ab2f3b39a
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2016-02-10 14:01:10 +0100

    use line comments consistently (local stylistic change)

--- test/files/bad/test_bad.js
+++ test/files/bad/test_bad.js
@@ -25,10 +25,8 @@ else {
 
 // We disallowMultipleLineBreaks
 
-/*
- * We disallowMultipleLineStrings, instead it should be handled like the
- * following
- */
+// We disallowMultipleLineStrings, instead it should be handled like the
+// following
 /* BAD
 var x =
 	"multi \
